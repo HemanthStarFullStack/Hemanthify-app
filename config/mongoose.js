b90699063087ed31dd1ed8621af815bc1bb9301e
SaiHemanthMandala <91903675+SaiHemanthMandala@users.noreply.github.com>
2021-12-13 16:32:17 +0530

    schema created

---
+++ config/mongoose.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost/hemanthify_development');
+
+const db  = mongoose.connection;
+
+db.on('error',console.error.bind(console,'error connecting to db'));
+
+db.once('open',function(){
+
+    console.log('connected to ::DB');
+
+});
+
+mongoose.exports = db;
+
--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const expressLayouts = require("express-ejs-layouts");
 const app = express();
 const port = 8000;
 // const expressLayouts = require('express-ejs-layouts');
-
+const db  = require('./config/mongoose');
  
  
 app.use(express.static('./assets'));
