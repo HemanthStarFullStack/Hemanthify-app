5a86167f3ff6a6e03eb6de2fb78d9b8829429947
SaiHemanthMandala <91903675+SaiHemanthMandala@users.noreply.github.com>
2021-12-14 19:35:11 +0530

    signUp added

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,3 +1,4 @@
+const User = require("../models/user")
 module.exports.profile = function(req,res){
     console.log("at user_con");
     return res.render('users',{
@@ -5,3 +6,44 @@ module.exports.profile = function(req,res){
     });
 }
 
+module.exports.signUp = function(req,res){
+    return res.render("user_signUp",{
+        title: "Hemanthify :: Sign Up"
+    })
+}
+
+module.exports.signIn = function(req,res){
+    return res.render("user_signIn",{
+        title: "Hemanthify :: Sign In "
+    })
+}
+
+module.exports.create = function(req,res){
+    console.log("body",req.body);
+    if(req.body.password != req.body.confirm_password){
+        return res.redirect('back');
+    }
+    User.findOne({email:req.body.email},function(err,user){
+        if(err){
+            console.log('not found');
+            return;
+        }
+        if(!user){
+            User.create(req.body,function(err,user){
+                if(err){
+                    console.log('sign up error');
+                    return;
+                }
+                return res.redirect('/users/sign-in');
+            })
+        }
+        else{
+            return res.redirect('back');
+
+        }
+    });
+
+}
+module.exports.createSession = function(req,res){
+    
+}
