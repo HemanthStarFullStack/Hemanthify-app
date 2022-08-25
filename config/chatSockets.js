787c6b86c2500de09b30a05221a3474e1b2ae54a
HemanthStarFullStack <saihemanthmandala1@gmail.com>
2022-08-25 04:17:08 -0800

    reverted back(issue status:unresolved(repeat attempt 1))

--- config/chatSockets.js
+++ config/chatSockets.js
@@ -6,7 +6,7 @@ module.exports.chatSockets = function(socketServer){
     let io = Server(socketServer, {
         // Fixing the cors issue
         cors: {
-            origin: "http://hemanthify.in"
+            origin: "https://hemanthify.in"
         }
     });
     io.sockets.on('connection', function(socket){
